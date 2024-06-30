e8f10d2de8f3cbe2ddaf9fb399471b78c854aa5e
VerifiedFemboy <maciekplaygamesyt@gmail.com>
2024-06-30 23:45:21 +0200

    [+] Account ID

--- src/account.rs
+++ src/account.rs
@@ -1,11 +1,13 @@
-use actix_web::{HttpResponse, post, web};
+use actix_web::{get, HttpResponse, post, web};
 use actix_web::web::Json;
+use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 
 use crate::database::Database;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Account {
+    pub _id: ObjectId,
     pub name: String,
     pub about: String,
     pub create_date: String,
@@ -18,10 +20,9 @@ impl Account {
         Ok("okie ;3")
     }
 
-    pub async fn new(name: String, about: String, create_date: String, auth_key: String) -> Self {
-        Self { name, about, create_date, auth_key }
+    pub fn new(_id: ObjectId, name: String, about: String, create_date: String, auth_key: String) -> Self {
+        Self { _id, name, about, create_date, auth_key }
     }
-
 }
 
 #[post("/account/create")]
@@ -30,7 +31,7 @@ pub async fn create_account(post_account_args: Json<Account>, database: web::Dat
     let name = post_account_args.name.to_string();
     let key = "generated".to_string();
 
-    let account = Account::new(name, "".to_string(), "".to_string(), key).await;
+    let account = Account::new(ObjectId::new(), name, "".to_string(), "".to_string(), key).await;
     match database.insert_account(account.clone()).await {
         Ok(_) => {
             HttpResponse::Ok().body(format!("Account created! This is your auth key: {}", account.auth_key.to_string()))
@@ -39,4 +40,9 @@ pub async fn create_account(post_account_args: Json<Account>, database: web::Dat
             HttpResponse::InternalServerError().body("Something went wrong while creating an Account!")
         }
     }
+}
+
+#[get("/account")]
+pub async fn get_account() -> HttpResponse {
+
 }
